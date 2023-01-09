0f7c0e83c333216bbfa58307aab6348f80467f8e
pandeyprashant123-coder <83771967+pandeyprashant123-coder@users.noreply.github.com>
2023-01-09 23:43:12 +0545

    task

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,13 +1,27 @@
-require("./db/connect")
 const express = require("express");
 const app = express();
 const task = require('./routes/task');
+const connectDB = require("./db/connect")
+const notFound = require("./middleware/not-found")
+const errorHandlerMiddleWare = require("./middleware/error-handler")
+require('dotenv').config()
 //middleware
-
+app.use(express.static('./public'))
 app.use(express.json());
 
 
 app.use('/api/v1/task', task);
+app.use(notFound)
+app.use(errorHandlerMiddleWare)
 const port = 3000;
 
-app.listen(port,console.log(`server is listenig at port ${port}....`));
+const start = async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`server is listenig at port ${port}....`));
+    }catch(err){
+        console.log(err)
+    }
+}
+
+start()
--- 03-task-manager/starter/db/connect.js
+++ 03-task-manager/starter/db/connect.js
@@ -1,9 +1,13 @@
 const mongoose = require("mongoose");
 
-const connectionString =
-  "mongodb+srv://pandey_don:#imposterno1@notexpressproject.pycq0.mongodb.net/03-task-manager?retryWrites=true&w=majority";
 
-mongoose
-  .connect(connectionString)
-  .then(() => console.log("connected to db..."))
-  .catch((err) => console.log(err));
+const conectDB =(url)=>{
+  return mongoose.connect(url,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true,
+  })
+}
+module.exports =conectDB
+  
